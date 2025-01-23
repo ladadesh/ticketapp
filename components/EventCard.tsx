964a6429969cfaa3003ecab73b9c3395243e9bcf
adesh <ladadesh@gmail.com>
2025-01-24 01:33:23 +0530

    EventCard integrated.

--- components/EventCard.tsx
+++ components/EventCard.tsx
@@ -3,16 +3,39 @@
 import { api } from "@/convex/_generated/api";
 import { Id } from "@/convex/_generated/dataModel";
 import { useUser } from "@clerk/nextjs";
-import { useRouter } from "@/node_modules/next/router";
+import { useRouter } from "next/navigation";
 import { useQuery } from "convex/react";
+import { useStorageUrl } from "@/lib/utils";
 
 const EventCard = ({ eventId }: { eventId: Id<"events"> }) => {
   const { user } = useUser();
-  //   const router = useRouter();
+  const router = useRouter();
   const event = useQuery(api.events.getEventById, { eventId });
+  const eventAvailablity = useQuery(api.events.getEventAvailablity, {
+    eventId,
+  });
+  const userTicket = useQuery(api.tickets.getUserTicketForEvent, {
+    eventId,
+    userId: user?.id ?? "",
+  });
+  const queuePosition = useQuery(api.waitingList.getQueuePosition, {
+    eventId,
+    userId: user?.id ?? "",
+  });
+  const imageUrl = useStorageUrl(event?.imageStorageId);
 
-  console.log("event", event);
+  if (!event || !eventAvailablity) {
+    return null;
+  }
 
-  return <div>EventCard</div>;
+  const isPastEvent = event.eventDate < Date.now();
+  const isEventOwner = user?.id === event?.userId;
+
+  return (
+    <div
+      onClick={() => router.push(`/event/${eventId}`)}
+      className={`bg-white rounded-xl shadow-sm hover:shadow-lg transition-all duration-300 border border-gray-100 cursor-pointer overflow-hidden relative ${isPastEvent ? "opacity-75 hover:opacity-100" : ""}`}
+    ></div>
+  );
 };
 export default EventCard;
